75baece33c06f17df09b572e44c4c9e8aed74ce3
Alexis Perez <45041860+Alecxz20@users.noreply.github.com>
2023-05-13 18:30:13 -0500

    UI finished

--- script.js
+++ script.js
@@ -1,74 +1,103 @@
-// // Computer randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-// function getComputerChoice() {
-//   const randomNumber = Math.floor(Math.random() * 3);
-//   if (randomNumber === 0) {
-//     return "rock";
-//   } else if (randomNumber === 1) {
-//     return "paper";
-//   } else if (randomNumber === 2) {
-//     return "scissors";
-//   }
-// }
+// Computer randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
+  }
+}
 
-// function fighting(playerSelection, computerSelection) {
-//   if (playerSelection === computerSelection) {
-//     return `It's a tie, you both have chosen ${playerSelection}`;
-//   } else {
-//     if (
-//       (playerSelection === "rock" && computerSelection === "scissors") ||
-//       (playerSelection === "paper" && computerSelection === "rock") ||
-//       (playerSelection === "scissors" && computerSelection === "paper")
-//     ) {
-//       return `You Win! ${playerSelection} beats ${computerSelection}`;
-//     } else {
-//       return `You Lose! ${computerSelection} beats ${playerSelection}`;
-//     }
-//   }
-// }
+// Getting user chose from DOM
+function getUserChoice(el) {
+  if (el.classList.contains("fa-hand-back-fist")) {
+    return "rock";
+  } else if (el.classList.contains("fa-paper-plane")) {
+    return "paper";
+  } else if (el.classList.contains("fa-scissors")) {
+    return "scissors";
+  }
+}
 
-// function game() {
-//   let userScore = 0;
-//   let computerScore = 0;
+// Result from comparing user and computer choice
+function fighting(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return `It's a tie, you both have chosen ${userChoice}`;
+  } else {
+    if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+      return `You Win! ${userChoice} beats ${computerChoice}`;
+    } else {
+      return `You Lose! ${computerChoice} beats ${userChoice}`;
+    }
+  }
+}
 
-//   for (let i = 0; i < 5; i++) {
-//     const computerChoice = getComputerChoice();
+function resetAll() {
+  btn.classList.add("hide");
+  userNumber = 0;
+  computerNumber = 0;
+  playerScore.innerHTML = userNumber;
+  computerScore.innerHTML = computerNumber;
+  text.removeChild(match);
+  text.innerHTML = "Are you ready to play?";
+  keepWorking = true;
+}
 
-//     const userChoice = prompt(
-//       "Chose 'rock' 'paper' or 'scissors'"
-//     ).toLowerCase();
-
-//     const result = fighting(userChoice, computerChoice);
-//     console.log(result);
-
-//     if (computerChoice === userChoice) {
-//       i--;
-//     } else {
-//       if (result[4] === "W") {
-//         userScore++;
-//       } else if (result[4] === "L") {
-//         computerScore++;
-//       }
-//     }
-//   }
-
-//   if (userScore > computerScore) {
-//     return `Congrats, you have won the game:
-//     you: ${userScore}
-//     computer: ${computerScore}`;
-//   } else {
-//     return `Oh no, you have lost the game:
-//     you: ${userScore}
-//     computer: ${computerScore}`;
-//   }
-// }
-
-// const final = game();
-// console.log(final);
-
-//DOM
 const elements = document.querySelectorAll(".element");
+const text = document.querySelector(".text");
+const playerScore = document.querySelector(".score__player__result");
+const computerScore = document.querySelector(".score__pc__result");
+const match = document.createElement("p");
+match.style = "font-size: 42px";
+const btn = document.querySelector(".replay");
+
+let userNumber = 0;
+let computerNumber = 0;
+let keepWorking = true;
+
+function doIContinue() {
+  if (computerNumber >= 5) {
+    keepWorking = false;
+  }
+  if (userNumber >= 5) {
+    keepWorking = false;
+  }
+}
+
+function afterClick(e) {
+  if (keepWorking) {
+    const computer = getComputerChoice();
+    const user = getUserChoice(e.currentTarget);
+    result = fighting(user, computer);
+
+    text.innerHTML = result;
+    if (result[4] === "W") {
+      userNumber++;
+    } else if (result[4] === "L") {
+      computerNumber++;
+    }
+    playerScore.innerHTML = userNumber;
+    computerScore.innerHTML = computerNumber;
+
+    if (userNumber == 5 || computerNumber == 5) {
+      userNumber == 5
+        ? (match.innerText = `You have won the match!`)
+        : (match.innerText = `You have lost the match!`);
+      text.appendChild(match);
+      btn.classList.remove("hide");
+    }
+    doIContinue();
+  } else {
+    btn.addEventListener("click", resetAll);
+  }
+}
+
 elements.forEach(function (item) {
-  item.addEventListener("click", function () {
-    console.log(item);
-  });
+  item.addEventListener("click", afterClick);
 });
