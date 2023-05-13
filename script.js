f4a51876a8f6194389384228ed90d51d53070b0c
Alexis Perez <45041860+Alecxz20@users.noreply.github.com>
2023-05-12 21:21:57 -0500

    create UI and add new functionality

--- script.js
+++ script.js
@@ -1,66 +1,74 @@
-// Computer randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber === 0) {
-    return "rock";
-  } else if (randomNumber === 1) {
-    return "paper";
-  } else if (randomNumber === 2) {
-    return "scissors";
-  }
-}
+// // Computer randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// function getComputerChoice() {
+//   const randomNumber = Math.floor(Math.random() * 3);
+//   if (randomNumber === 0) {
+//     return "rock";
+//   } else if (randomNumber === 1) {
+//     return "paper";
+//   } else if (randomNumber === 2) {
+//     return "scissors";
+//   }
+// }
 
-function fighting(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    return `It's a tie, you both have chosen ${playerSelection}`;
-  } else {
-    if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
-  }
-}
+// function fighting(playerSelection, computerSelection) {
+//   if (playerSelection === computerSelection) {
+//     return `It's a tie, you both have chosen ${playerSelection}`;
+//   } else {
+//     if (
+//       (playerSelection === "rock" && computerSelection === "scissors") ||
+//       (playerSelection === "paper" && computerSelection === "rock") ||
+//       (playerSelection === "scissors" && computerSelection === "paper")
+//     ) {
+//       return `You Win! ${playerSelection} beats ${computerSelection}`;
+//     } else {
+//       return `You Lose! ${computerSelection} beats ${playerSelection}`;
+//     }
+//   }
+// }
 
-function game() {
-  let userScore = 0;
-  let computerScore = 0;
+// function game() {
+//   let userScore = 0;
+//   let computerScore = 0;
 
-  for (let i = 0; i < 5; i++) {
-    const computerChoice = getComputerChoice();
+//   for (let i = 0; i < 5; i++) {
+//     const computerChoice = getComputerChoice();
 
-    const userChoice = prompt(
-      "Chose 'rock' 'paper' or 'scissors'"
-    ).toLowerCase();
+//     const userChoice = prompt(
+//       "Chose 'rock' 'paper' or 'scissors'"
+//     ).toLowerCase();
 
-    const result = fighting(userChoice, computerChoice);
-    console.log(result);
+//     const result = fighting(userChoice, computerChoice);
+//     console.log(result);
 
-    if (computerChoice === userChoice) {
-      i--;
-    } else {
-      if (result[4] === "W") {
-        userScore++;
-      } else if (result[4] === "L") {
-        computerScore++;
-      }
-    }
-  }
+//     if (computerChoice === userChoice) {
+//       i--;
+//     } else {
+//       if (result[4] === "W") {
+//         userScore++;
+//       } else if (result[4] === "L") {
+//         computerScore++;
+//       }
+//     }
+//   }
 
-  if (userScore > computerScore) {
-    return `Congrats, you have won the game: 
-    you: ${userScore}
-    computer: ${computerScore}`;
-  } else {
-    return `Oh no, you have lost the game: 
-    you: ${userScore}
-    computer: ${computerScore}`;
-  }
-}
+//   if (userScore > computerScore) {
+//     return `Congrats, you have won the game:
+//     you: ${userScore}
+//     computer: ${computerScore}`;
+//   } else {
+//     return `Oh no, you have lost the game:
+//     you: ${userScore}
+//     computer: ${computerScore}`;
+//   }
+// }
 
-const final = game();
-console.log(final);
+// const final = game();
+// console.log(final);
+
+//DOM
+const elements = document.querySelectorAll(".element");
+elements.forEach(function (item) {
+  item.addEventListener("click", function () {
+    console.log(item);
+  });
+});
